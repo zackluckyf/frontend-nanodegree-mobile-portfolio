2f359ff1fb6912c123c15149ad7b5f071136985a
zackluckyf <fanning.34@gmail.com>
2016-10-24 21:09:55 -0400

    moved inline js into external file and added width and height to 4 images in index.html

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -2,13 +2,24 @@
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
 function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+    var t = window.performance.timing,
+        dcl = t.domContentLoadedEventStart - t.domLoading,
+        complete = t.domComplete - t.domLoading;
+    var stats = document.getElementById("crp-stats");
+    stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
 window.addEventListener("load", function(event) {
-  logCRP();
+    logCRP();
 });
+(function(w, g) {
+    w.GoogleAnalyticsObject = g;
+    w[g] = w[g] || function() {
+        (w[g].q = w[g].q || []).push(arguments);
+    };
+    w[g].l = 1 * new Date();
+})(window, 'ga');
+
+// Optional TODO: replace with your Google Analytics profile ID.
+ga('create', 'UA-XXXX-Y');
+ga('send', 'pageview');
